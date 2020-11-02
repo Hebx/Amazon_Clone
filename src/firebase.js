8d0abc44bbc1aff862f1e5498995f3c866d67b52
Hebx <lord.noctx@gmail.com>
2020-11-02 23:53:11 +0000

    Amazon Clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8nHLq-RZBnPec1YpoAsn0qPPCJvegHRU",
+    authDomain: "clone-b3dc9.firebaseapp.com",
+    databaseURL: "https://clone-b3dc9.firebaseio.com",
+    projectId: "clone-b3dc9",
+    storageBucket: "clone-b3dc9.appspot.com",
+    messagingSenderId: "894625182820",
+    appId: "1:894625182820:web:81cc1d36be2555df8a5b71",
+    measurementId: "G-6BELDNWM53"
+  };
